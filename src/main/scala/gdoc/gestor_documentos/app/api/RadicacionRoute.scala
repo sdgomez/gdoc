5054f237936332dde6cfad089ac613060761170b
Samuel Gómez <samuelgomez@seven4n.com>
2017-12-25 14:48:39 -0500

    se hace el coco para los servicios en la api

--- src/main/scala/gdoc/gestor_documentos/app/api/RadicacionRoute.scala
+++ src/main/scala/gdoc/gestor_documentos/app/api/RadicacionRoute.scala
@@ -1,13 +1,39 @@
 package gdoc.gestor_documentos.app.api
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.http.scaladsl.server.Directives._
+import gdoc.gestor_documentos.model.InternoDTO
 
 trait RadicacionRoute {
   val route =
-    path("hello") {
-      get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+    pathPrefix("radicarInterno") {
+      post {
+        entity(as[InternoDTO]){
+          internoDTO =>
+            complete(
+              "ok"
+            )
+        }
+
       }
+    } ~ pathPrefix("radicarExterno") {
+    post {
+      entity(as[InternoDTO]){
+        internoDTO =>
+          complete(
+            "ok"
+          )
+      }
+
     }
+  } ~ pathPrefix("radicarRecibido") {
+    post {
+      entity(as[InternoDTO]){
+        internoDTO =>
+          complete(
+            "ok"
+          )
+      }
+
+    }
+  }
 }
